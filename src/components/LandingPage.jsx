ca9533ecb347d7fb410d9eb1507d4a5129cd1d53
Maco <maco@imac-de-maco.home>
2018-10-05 14:10:59 +0200

    Small Screen Responsive done

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import VideoCover from "react-video-cover";
 import homeVid from "../assets/videos/homeVid.mp4";
 import iphone from "../assets/images/iPhone.png"
+import { Row, Col } from 'antd';
 import "./LandingPage.css"
 const videoOptions = {
     src: homeVid,
@@ -26,37 +27,64 @@ class LandingPage extends React.Component {
                         videoOptions={videoOptions}
                     />
                     <div className="TitleContainer">
-                        <h2 >Collective.</h2>
+                        <h2 className="PageTitle">Collective.</h2>
                     </div>
                     <div className="Gradient">
                     </div>
-                    <div className="TextContainer">
-                        <h3 className="AppTitle">Book together right from your group chat</h3>
-                        <p className="AppText">Book together your festival tickets, accommodation and transfers, from a simple conversation with your friends. Instantly split the payment.</p>
-                    </div>
+                        <div className="TextContainer">
+                        <Row >
+                        <Col xs={{ span: 24 }} sm={{ span: 4 }} md={{ span: 20, offset: 4}}>
+                            <h3 className="AppTitle">Book together right from your group chat</h3>
+                            <p className="AppText">Book together your festival tickets, accommodation and transfers, from a simple conversation with your friends. Instantly split the payment.</p>
+                            </Col>
+                        </Row>
+                        </div>
+
                     <div className="PhoneContainer">
-                        <img src={iphone} alt="iphone"/>
+                        <img className="PhoneImage" src={iphone} alt="iphone"/>
                     </div>
                     <div className="FormContainer">
-                        <form>
-                            <label>
-                                <input className="InputStyle" type="text" name="name" placeholder="Your Favorite Festival" />
-                            </label>
-                            <label>
-                                <input className="InputStyle" type="text" name="name" placeholder="Your e-mail" />
-                            </label>
-                            <input className="InputButtonStyle" type="submit" value="WIN 4 PASSES" />
-                        </form>
+                        
+                            <form>
+                            <Row gutter={16}>
+                                <Col xs={{ span: 24 }} sm={{ span: 5 }} lg={{ span: 8 }}  >
+                                    <label>
+                                        <input className="InputStyle" type="text" name="name" placeholder="Your Favorite Festival" />
+                                    </label>
+                                </Col>
+                                <Col xs={{ span: 24 }} sm={{ span: 5, offset: 1 }} lg={{ span: 8, offset: 1 }}>
+                                    <input className="InputStyle" type="text" name="name" placeholder="Your e-mail" />
+                                </Col>
+                                <Col xs={{ span: 24 }} sm={{ span: 3, offset: 1 }} lg={{ span: 6, offset: 1 }}>
+                                <input className="InputButtonStyle" type="submit" value="WIN 4 PASSES" />
+                                </Col>
+                                </Row>
+                            </form>
+                        
                     </div>
             </div>
             <div className="Footer">
+            <Row >
             <div className="FooterContent">
-            <p className="Logo">Collective<span className="LogoDot">.</span></p>
+                <Col  lg={{ span: 5, offset: -5 }} >
+                <p className="Logo">Collective<span className="LogoDot">.</span></p>
+                </Col>
+                <Col xs={{ span: 4, offset: 1 }} md={{ span: 4, offset: 5 }} >
                 <p className="FooterText">40 rue de l'arsenal - 33 300 Bordeaux</p>
-                <p className="FooterText">hello@collective.com</p>
-                <p className="FooterText">+33 6 23 56 12 87</p>
-            </div>
+                </Col>
                 
+                
+                <Col xs={{ span: 4, offset: 1}} md={{ span: 4 }} ><p className="FooterText">hello@collective.com</p>
+                </Col>
+                
+                <Col xs={{ span: 4, offset: 1 }} md={{ span: 4   }} >
+                <p className="FooterText">+33 6 23 56 12 87</p>
+                </Col>
+                
+            
+
+            </div>
+            </Row>
                 <p className="CopyRight">© CLEZER Financial Technologie INC. All Rights Reserved.</p>
             </div>
             
