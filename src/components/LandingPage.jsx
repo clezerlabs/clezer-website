59ae57778e0dfdd935f431b44e87c7dbe443cfe7
Maco <maco@imac-de-maco.home>
2018-10-08 14:10:57 +0200

    V1 site done

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import VideoCover from "react-video-cover";
 import homeVid from "../assets/videos/homeVid.mp4";
 import iphone from "../assets/images/iPhone.png"
+import fb from "../assets/images/fb.png"
+import twitter from "../assets/images/twitter.png"
+
 import { Row, Col } from 'antd';
 import "./LandingPage.css"
 const videoOptions = {
@@ -27,16 +30,20 @@ class LandingPage extends React.Component {
                         videoOptions={videoOptions}
                     />
                     <div className="TitleContainer">
-                        <h2 className="PageTitle">Collective.</h2>
+                        <h2 className="PageTitle">Clezer.</h2>
                     </div>
                     <div className="Gradient">
                     </div>
                         <div className="TextContainer">
                         <Row >
-                        <Col xs={{ span: 24 }} sm={{ span: 4 }} md={{ span: 20, offset: 4}}>
+                        <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 24}}>
                             <h3 className="AppTitle">Book together right from your group chat</h3>
+                        </Col>
+                        </Row>
+                        <Row >
+                        <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 24}}>
                             <p className="AppText">Book together your festival tickets, accommodation and transfers, from a simple conversation with your friends. Instantly split the payment.</p>
-                            </Col>
+                        </Col>
                         </Row>
                         </div>
 
@@ -46,16 +53,16 @@ class LandingPage extends React.Component {
                     <div className="FormContainer">
                         
                             <form>
-                            <Row gutter={16}>
-                                <Col xs={{ span: 24 }} sm={{ span: 5 }} lg={{ span: 8 }}  >
+                            <Row gutter={8}>
+                                <Col xs={{ span: 12 }} sm={{ span: 7 }} lg={{ span: 7 }}  >
                                     <label>
                                         <input className="InputStyle" type="text" name="name" placeholder="Your Favorite Festival" />
                                     </label>
                                 </Col>
-                                <Col xs={{ span: 24 }} sm={{ span: 5, offset: 1 }} lg={{ span: 8, offset: 1 }}>
+                                <Col xs={{ span: 12 }} sm={{ span: 7 }} lg={{ span: 7 }}>
                                     <input className="InputStyle" type="text" name="name" placeholder="Your e-mail" />
                                 </Col>
-                                <Col xs={{ span: 24 }} sm={{ span: 3, offset: 1 }} lg={{ span: 6, offset: 1 }}>
+                                <Col xs={{ span: 24 }} sm={{ span: 7 }} lg={{ span: 7 }}>
                                 <input className="InputButtonStyle" type="submit" value="WIN 4 PASSES" />
                                 </Col>
                                 </Row>
@@ -64,20 +71,23 @@ class LandingPage extends React.Component {
                     </div>
             </div>
             <div className="Footer">
+            <Row>
+                <Col xs={{ span: 10, offset: 9}} >
+                <p className="Logo">Clezer<span className="LogoDot">.</span></p>
+                </Col>
+            </Row>
             <Row >
             <div className="FooterContent">
-                <Col  lg={{ span: 5, offset: -5 }} >
-                <p className="Logo">Collective<span className="LogoDot">.</span></p>
-                </Col>
-                <Col xs={{ span: 4, offset: 1 }} md={{ span: 4, offset: 5 }} >
-                <p className="FooterText">40 rue de l'arsenal - 33 300 Bordeaux</p>
+                
+                <Col xs={{ span: 6, offset: 1 }} md={{ span: 4, offset: 5 }} >
+                <p className="FooterText">40 rue de l'arsenal  33300 Bordeaux</p>
                 </Col>
                 
                 
-                <Col xs={{ span: 4, offset: 1}} md={{ span: 4 }} ><p className="FooterText">hello@collective.com</p>
+                <Col xs={{ span: 6, offset: 2}} md={{ span: 4 }} ><p className="FooterText">hello@collective.com</p>
                 </Col>
                 
-                <Col xs={{ span: 4, offset: 1 }} md={{ span: 4   }} >
+                <Col xs={{ span: 6, offset: 2 }} md={{ span: 4   }} >
                 <p className="FooterText">+33 6 23 56 12 87</p>
                 </Col>
                 
@@ -85,6 +95,12 @@ class LandingPage extends React.Component {
 
             </div>
             </Row>
+                <div className="footerLogo">
+                    <a className="logoFb" href="https://www.facebook.com/clezer/"><img className="logoFb" src={fb} alt="facebookLogo"/></a>
+                    <a className="logoTwitter" href="https://twitter.com/clezerapp?lang=fr"><img className="logoTwitter" src={twitter} alt="facebookLogo"/></a>
+                </div>
+                    
+           
                 <p className="CopyRight">© CLEZER Financial Technologie INC. All Rights Reserved.</p>
             </div>
             
